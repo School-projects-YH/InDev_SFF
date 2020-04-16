77ef8f72c4d9a0f8691f913d627b4e64e62bb2fa
Lukas Selin <lukas.selin@viskan.se>
2020-04-16 13:27:44 +0200

    Initial commit

---
+++ SFF.Tests/MovieTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using SFF.Models;
+using AutoFixture;
+using SFF.Controllers;
+
+namespace SFF.Tests
+{
+    public class MovieTests
+    {
+        [Fact]
+        public void Create_a_movie()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var movieContext = fixture.Create<MovieContext>();
+
+            var sut = new MovieController(movieContext);
+
+            // Act
+            sut.PostMovie()
+            
+            // Assert
+        }
+    }
+}
---
+++ SFF/Models/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SFF.Models
+{
+    public class Movie
+    {
+        public Movie(string Title, decimal Duration)
+        {
+
+        }
+
+
+        [Key]
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Duration
+        {
+            get { return DurationFormat(); }
+            set
+            {
+                DurationInMinutes = Int32.Parse(value);
+            }
+        }
+        private int DurationInMinutes { get; set; }
+
+        private string DurationFormat()
+
+        {
+            float dim = DurationInMinutes;
+            double hours = Math.Truncate(dim / 60f);
+            double minutes = dim - (hours * 60);
+
+            string returnString = hours.ToString() + ':' + minutes.ToString();
+
+            return returnString;
+        }
+    }
+
+}
